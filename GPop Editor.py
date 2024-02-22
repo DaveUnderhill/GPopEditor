e747d21ffef54774515a8573bfec339cb26c8423
Dave Underhill <david@scharf-web.de>
2024-02-22 02:46:19 +0100

    test

---
+++ GPop Editor.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
+
+
+class NoteTimelineWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Noten Timeline')
+        self.setGeometry(100, 100, 800, 600)
+
+        self.init_ui()
+
+    def init_ui(self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
+
+        label = QLabel('Hier könnte Ihre Noten-Timeline sein!')
+        label.setAlignment(QtCore.Qt.AlignCenter)  # Beachten Sie die Verwendung von QtCore.Qt
+        layout.addWidget(label)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = NoteTimelineWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
